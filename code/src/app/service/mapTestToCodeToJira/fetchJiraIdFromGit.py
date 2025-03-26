dab997e5d1e43fae5d753594a2e6f47d0018918f
msnish <58057944+msnish@users.noreply.github.com>
2025-03-26 18:23:06 +0530

    Add files via upload

--- code/src/app/service/mapTestToCodeToJira/fetchJiraIdFromGit.py
+++ code/src/app/service/mapTestToCodeToJira/fetchJiraIdFromGit.py
@@ -1,5 +1,7 @@
 import subprocess
 import re
+import os
+import json
 
 def get_jira_from_commit():
     commit_message = subprocess.run(
@@ -8,7 +10,18 @@ def get_jira_from_commit():
     ).stdout
     match = re.search(r"(JIRA-\d+)", commit_message)
     jira = match.group(0) if match else None
-    return jira
+    #return jira
+
+    # Open and load the jira.json file
+    with open(jira.json, "r") as jira_file:
+        jira_data = json.load(jira_file)
+        #print(jira_data)
+
+    if jira in jira_data:
+        story = jira_data[jira].get("story")
+        return f"JIRA ID: {jira}, Story: {story}" if story else f"JIRA ID: {jira}, Story not found."
+    else:
+        return f"JIRA ID: {jira} not found in jira.json."   
 
 print(get_jira_from_commit())  # JIRA-1234
 
